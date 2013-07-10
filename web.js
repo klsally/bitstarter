c223a15366532544281ff859821519932cf5638c
Kevin Sally <klsally@gmail.com>
2013-07-10 03:33:06 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require('express'), fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(256);
+buffer = fs.readFileSync("index.html");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
